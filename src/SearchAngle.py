f9524658d885f42ebf57aba163b69c8e6d9fd72f
lgarciaorozco6 <lgarciaorozco6@gmail.com>
2026-02-03 23:35:38 -0600

    Search advance 0

---
+++ src/SearchAngle.py
@@ -0,0 +1,23 @@
+from Figures import Fig1_V4, Fig2_V4, Fig1_V2, Fig2_V2, Fig1_V3, Fig2_V3
+from new_rep import SemiDummyTriangle as Triangle
+
+
+class SearchGraph:
+   def __init__(self, root, Fig):
+      self.root = root
+      self.Fig = Fig
+   
+   def get_siblings(self, v):
+      siblings = []
+      
+      
+
+
+def Search_V4(angle, Fig):
+   steps = 0
+   
+   
+   
+   
+   res = None
+   return res
