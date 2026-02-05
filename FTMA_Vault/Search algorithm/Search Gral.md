4decac4a2dd41b7c309c9a437a9b12e13bcc81bf
lgarciaorozco <lgarciaorozco6@gmail.com>
2026-02-04 18:25:34 -0600

    Search advamce 1

---
+++ FTMA_Vault/Search algorithm/Search Gral.md
@@ -0,0 +1,14 @@
+# Workflow: Two ways
+
+## From the known angles (with an heuristic)
+- [ ] Choose **heuristic**
+
+- [ ] Get neighbors 
+- [ ] Sort with heuristic
+- [ ] Explore next
+- [ ] Repeat
+
+## From the missing angles (until something is found)
+- [ ] Get neighbors
+- [ ] Explore the one with more known neighbors
+- [ ] Repeat
--- src/SearchAngle.py
+++ src/SearchAngle.py
@@ -1,17 +1,41 @@
 from Figures import Fig1_V4, Fig2_V4, Fig1_V2, Fig2_V2, Fig1_V3, Fig2_V3
 from new_rep import SemiDummyTriangle as Triangle
+from itertools import combinations
 
 
 class SearchGraph:
    def __init__(self, root, Fig):
       self.root = root
       self.Fig = Fig
+      lines = Fig.lines
+      angles = Fig.angles
+      triangles = Fig.triangles
+      
+   def neighbors(self, angle):
+      vertex_neighbours = []
+      triangles_neigbours = []
+      
+      center = angle[1]
+      lines = self.lines_with(center)
+      extremes = [set(l[0],l[1]) - set(center) for l in lines]
+      for l1, l2 in combinations(extremes):
+         for e in ext1: 
+         
+      
    
-   def get_siblings(self, v):
-      siblings = []
+   def lines_with(self, point):
+      lines = []
+      for line in self.lines.values():
+         if point in line:
+            lines.append(point)
+      return lines
       
-      
-
+   def identify_line(self, name):
+      for k, line in self.Fig.lines.items():
+         if len(set(line) & set(name)) == 2:
+            return k
+   
+   def 
 
 def Search_V4(angle, Fig):
    steps = 0
